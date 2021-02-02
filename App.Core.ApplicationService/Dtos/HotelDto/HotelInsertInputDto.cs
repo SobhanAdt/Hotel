52e7f9566ae1ad99e6b41fc8d3220459ecbecf27
Your Name <you@example.com>
2021-02-02 11:53:12 +0330

    .

--- App.Core.ApplicationService/Dtos/HotelDto/HotelInsertInputDto.cs
+++ App.Core.ApplicationService/Dtos/HotelDto/HotelInsertInputDto.cs
@@ -11,7 +11,9 @@ namespace App.Core.ApplicationService.Dtos.HotelDto
         [Required(ErrorMessage = "نام هتل حتما باید وارد شود")]
         public string HotelName { get; set; }      
         public int HotelCode { get; set; }
-        public int RoomCount { get; set; }  
+        public int RoomCount { get; set; }
+        public string Description { get; set; }
+
         public int CityId { get; set; }
         public int RateId { get; set; }
 
--- App.Core.ApplicationService/Dtos/HotelDto/HotelUpdateInputDto.cs
+++ App.Core.ApplicationService/Dtos/HotelDto/HotelUpdateInputDto.cs
@@ -12,6 +12,7 @@ namespace App.Core.ApplicationService.Dtos.HotelDto
         public string HotelName { get; set; }
         public int HotelCode { get; set; }
         public int RoomCount { get; set; }
+        public string Description { get; set; }
         public int CityId { get; set; }
         public int RateId { get; set; }
 

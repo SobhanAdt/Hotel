ba396205321b9a0bf64cb2d6f906dd26e1e82d66
Your Name <you@example.com>
2021-02-05 09:33:41 +0330

    .

---
+++ App.Core.ApplicationService/Dtos/UserRateDto/UserRateUpdateDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.Core.ApplicationService.Dtos.USerRateDto
+{
+    public class UserRateUpdateDto
+    {
+        [Display(Name = "ای دی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public int Id { get; set; }
+
+        [Display(Name = "امتیاز")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public float RateNumber { get; set; }
+
+        [Display(Name = "ای دی کاربر")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public int UserId { get; set; }
+
+
+        [Display(Name = "ای دی هتل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public int HotelId { get; set; }
+    }
+}

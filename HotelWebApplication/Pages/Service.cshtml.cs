a8c929866d43757ffb19c504c82079bd1190c7e7
shadi_rahbar74@yahoo.com <shadi_rahbar74@yahoo.com>
2021-02-21 14:21:33 +0330

    ....

--- HotelWebApplication/Pages/Service.cshtml.cs
+++ HotelWebApplication/Pages/Service.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using App.Core.ApplicationService.ApplicationServices.Hotel;
+using App.Core.ApplicationService.Dtos.HotelDto;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,16 @@ namespace HotelWebApplication.Pages
 {
     public class ServiceModel : PageModel
     {
+        private readonly IHotelService hotelService;
+        public ServiceModel(IHotelService hotelService )
+        {
+            this.hotelService = hotelService;
+        }
+        [BindProperty]
+        public List<HotelGetOutPutDto> Hotels { get; set; }
         public void OnGet()
         {
+            Hotels = hotelService.GetAllHotels();
         }
     }
 }

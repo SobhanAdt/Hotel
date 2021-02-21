02c4205568232ccb324fe1573949d7de2b78ca80
SoBHaN <lSoBHaN@DESKTOP-D70UNE6>
2021-02-21 16:26:14 +0330

    .

---
+++ HotelWebApplication/Pages/FilterByStar.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App.Core.ApplicationService.ApplicationServices.Rate;
+using App.Core.ApplicationService.Dtos.StarDto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HotelWebApplication.Pages
+{
+    public class FilterByStarModel : PageModel
+    {
+        private readonly IStarService service;
+
+        public FilterByStarModel(IStarService service)
+        {
+            this.service = service;
+        }
+
+        [BindProperty]
+        public Task<List<StarOutputDto>> ListStar { get; set; }
+        public void OnGet()
+        {
+            ListStar = service.GetAllRate();
+        }
+    }
+}

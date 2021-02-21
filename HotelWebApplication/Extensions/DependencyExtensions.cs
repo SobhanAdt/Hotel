a5cbc573071c3bab483b8049b2d4a2bfee5fc769
SoBHaN <lSoBHaN@DESKTOP-D70UNE6>
2021-02-21 13:42:17 +0330

    .

---
+++ HotelWebApplication/Extensions/DependencyExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App.Core.ApplicationService.ApplicationServices.City;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HotelWebApplication.Extensions
+{
+    public static class DependencyExtensions
+    {
+        public static void AddDependency(this IServiceCollection service)
+        {
+            AddService(service);
+        }
+
+        public static void AddService(IServiceCollection service)
+        {
+            service.AddTransient<ICityService, CityService>();
+        }
+    }
+}
---
+++ HotelWebApplication/Pages/FilterByCity.cshtml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App.Core.ApplicationService.ApplicationServices.City;
+using App.Core.ApplicationService.Dtos.CityDto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HotelWebApplication.Pages
+{
+    public class FilterByCityModel : PageModel
+    {
+        //private readonly ICityService service;
+
+        //public FilterByCityModel(ICityService service)
+        //{
+        //    this.service = service;
+        //}
+
+        
+        public void OnGet()
+        {
+
+        }
+    }
+}

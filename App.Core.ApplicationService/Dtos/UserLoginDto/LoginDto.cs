39cb454551c2c4eb75e97a380b3d8b5c0a024e13
shadi_rahbar74@yahoo.com <shadi_rahbar74@yahoo.com>
2021-02-28 18:16:44 +0330

    ...

--- App.Core.ApplicationService/Dtos/UserLoginDto/LoginDto.cs
+++ App.Core.ApplicationService/Dtos/UserLoginDto/LoginDto.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace App.Core.ApplicationService.Dtos.UserLoginDto
 {
     public class LoginDto
     {
+        [Display(Name = "ایمیل آدرس")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [EmailAddress(ErrorMessage = "فرمت ایمیل صحیح نمی باشد")]
         public string Email { get; set; }
 
+        [Display(Name = "رمز عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
         public string Password { get; set; }
     }
 }

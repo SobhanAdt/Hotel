443d8fc16b47b1805c8bf5ad0f18ad880c9fbf27
shadi_rahbar74@yahoo.com <shadi_rahbar74@yahoo.com>
2021-03-01 15:10:36 +0330

    ...

---
+++ HotelWebApplication/Pages/User/UserInfoDetails.cshtml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App.Core.ApplicationService.ApplicationServices.User;
+using App.Core.ApplicationService.Dtos.Userto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HotelWebApplication.Pages.User
+{
+    public class UserInfoDetailsModel : PageModel
+    {
+        private IUserService userService;
+        public UserInfoDetailsModel(IUserService userService)
+        {
+            this.userService = userService;
+        }
+        [BindProperty]
+        public UserOutputDto user { get; set; }
+        public async Task OnGet(int Id)
+        {
+
+            user = await userService.GetSingelUser(Id);
+        }
+    }
+}

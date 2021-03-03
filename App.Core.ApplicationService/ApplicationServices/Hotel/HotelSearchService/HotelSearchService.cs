96df175a35f73cc6f578ee53f5dcf24369f0109a
shadi_rahbar74@yahoo.com <shadi_rahbar74@yahoo.com>
2021-03-03 20:44:54 +0330

    ...

---
+++ App.Core.ApplicationService/ApplicationServices/Hotel/HotelSearchService/HotelSearchService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using App.Core.ApplicationService.Dtos.HotelDto;
+using App.Core.ApplicationService.IRepositories;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hotel.Core.ApplicationService.ApplicationServices.Hotel
+{
+    public class HotelSearchService : IHotelSearchService
+    {
+        private IRepository<App.Core.Entities.Hotel> repository;
+        private IRepository<App.Core.Entities.UserRate> userRateRepository;
+        public HotelSearchService(IRepository<App.Core.Entities.Hotel> repository)
+        {
+            this.repository = repository;
+        }
+        public  Task<List<HotelGetOutPutDto>> SearchByHotelName(string name)
+        {
+           return repository.GetQuery().Include(x => x.City).Where(x => x.HotelName.Contains(name))
+                .Select(x => new HotelGetOutPutDto()
+                {
+                    HotelName = x.HotelName,
+                    CityName = x.City.CityName
+                }).ToListAsync(); 
+        }
+    }
+}
---
+++ App.Core.ApplicationService/ApplicationServices/Hotel/HotelSearchService/IHotelSearchService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using App.Core.ApplicationService.Dtos.HotelDto;
+
+namespace Hotel.Core.ApplicationService.ApplicationServices.Hotel
+{
+   public interface IHotelSearchService
+    {
+        public Task<List<HotelGetOutPutDto>> SearchByHotelName(string name);
+    }
+}
